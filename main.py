64a2f0168d94acef7302c092ea162bb1be7d31e6
UfoX59 <alexd59@mail.ru>
2025-03-05 20:59:05 +0500

    add cycle code

--- main.py
+++ main.py
@@ -25,10 +25,15 @@ while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            mouse_x, mouse_y = pygame.mouse.get_pos()
+            if target_x < mouse_x < target_x + target_wight and target_y < mouse_y < target_y + target_height:
+                target_x = random.randint(0, SCREEN_WIDTH - target_wight)
+                target_y = random.randint(0, SCREEN_HEIGHT - target_height)
+                color = (random.randint(0, 255), random.randint(0, 255), random.randint(0, 255))
+                screen.fill(color)
 
     screen.blit(target_img, (target_x, target_y))
     pygame.display.update()
-
-
 
 pygame.quit()
